a7d9161a1cfe34941ac519473957af1cd1f9d806
tsadpbb <tsadpbb@protonmail.com>
2020-01-26 07:56:22 -0600

    fix location again

--- location-services/location.py
+++ location-services/location.py
@@ -8,17 +8,6 @@ class Address:
     def __init__(self):
         self.key = os.getenv("API_KEY")
         self.client = gm.Client(self.key)
-
-    def findlatlong(self):
-        self.geoValue = self.client.geolocate()
-        self.latitude = self.geoValue['location']['lat']
-        self.longitude = self.geoValue['location']['lng']
-        return (self.latitude, self.longitude)
-
-    def getAddress(self):
-        self.findlatlong()
-        self.myAddress = self.client.reverse_geocode((self.latitude, self.longitude))
-        return self.myAddress[0]['formatted_address']
         self.getAddress()
 
     def getAddress(self):
