18125a43005d093caf0ccd0e89b734fa88597c17
tsadpbb <tsadpbb@protonmail.com>
2020-01-26 00:48:00 -0600

    finish location

--- location-services/location.py
+++ location-services/location.py
@@ -7,14 +7,13 @@ class Address:
     def __init__(self):
         self.key = os.getenv("API_KEY") 
         self.client = gm.Client(self.key)
+        self.getAddress()
     
-    def findlatlong(self):
-        self.geoValue = self.client.geolocate()
-        self.latitude = self.geoValue['location']['lat']
-        self.longitude = self.geoValue['location']['lng']
-        return (self.latitude, self.longitude)
-
     def getAddress(self):
-        self.findlatlong()
-        self.myAddress = self.client.reverse_geocode((self.latitude, self.longitude)) 
-        return self.myAddress[0]['formatted_address']
+        self.geoValue = self.client.geolocate()
+        self.latlong = (self.geoValue['location']['lat'], self.geoValue['location']['lng'])
+        self.addr = self.client.reverse_geocode(self.latlong)
+        print('hello')
+        return self.addr[0]['formatted_address']
+
+addr = Address()
