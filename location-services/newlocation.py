54f98040ba1c9587ad036c6e6585dd6eed71ca7c
tsadpbb <tsadpbb@protonmail.com>
2020-01-26 05:27:49 -0600

    clean up newlocation

--- location-services/newlocation.py
+++ location-services/newlocation.py
@@ -1,28 +1,17 @@
 import googlemaps as gm
 import os
 from dotenv import load_dotenv
-load_dotenv()
-key = os.getenv("API_KEY")
 from location import Address
 import sys
+
+load_dotenv()
+key = os.getenv("API_KEY")
 gmaps = gm.Client(key) 
-my_dist = gmaps.distance_matrix('Delhi','Mumbai')['rows'][0]['elements'][0] 
-loc1 = Address.getAddress()
-loc2 = sys.argv[1]
-def get_distance(loc1,loc2):
-    return gmaps.distance_matrix(loc1,loc2)['rows'][0]['elements'][0]
+loc1 = sys.argv[1]
+loc2 = sys.argv[2]
 
-def check_closeness(distmatrix):
-    if distmatrix['distance']['value'] <= 1600:
-        print('true')
+def checkIfClose(loc1, loc2):
+    distanceMatrix = gmaps.distance_matrix(loc1,loc2)['rows'][0]['elements'][0]
+    if distanceMatrix['distance']['value'] <= 1600:
         return True
-    else:
-        print('true1223')
-        return False
-def get_directions(loc1,loc2):
-    if check_closeness(get_distance(loc1,loc2)) == True:
-        return gmaps.directions(loc1,loc2)
-    return False
-
-
 
