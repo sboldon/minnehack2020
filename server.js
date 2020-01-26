50b793c2d1d96df13ae8d928e015156a7ca7dd65
Sam Boldon <samboldon@gmail.com>
2020-01-26 00:08:25 -0600

    attempt build fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const testAPIRouter = require('./routes/test-api');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 
 app.use(express.json()); // essentially bodyParser
 
