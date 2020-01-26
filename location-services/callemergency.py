b5b54722ab86551c333d121880da06c392a93a15
tsadpbb <tsadpbb@protonmail.com>
2020-01-26 03:14:54 -0600

    make callemergency

--- location-services/callemergency.py
+++ location-services/callemergency.py
@@ -1,1 +1,15 @@
 from location import Address
+import gsmmodem
+import sys
+
+class messageToSend:
+    def __init__(self, sys.arg[1]):
+        self.listOfPhones = sys.arg[1]
+        myAddressObject = Address()
+        myAddress = myAddressObject.getAddress()
+        myMessage = 'I need narcan at ' + str(myAddress)
+        sendingSMS(self, self.listOfPhones)
+
+    def sendingSMS(self, listPhones):
+        for phones in listPhones:
+            gsmmodem.sendSms(destination=phones, text=myMessage)
