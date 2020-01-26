2df9b374c5dc71d07ca726539c2c4ccd87990ca8
tsadpbb <tsadpbb@protonmail.com>
2020-01-26 05:48:04 -0600

    fix newlocation so that it actually works

--- location-services/newlocation.py
+++ location-services/newlocation.py
@@ -10,8 +10,9 @@ gmaps = gm.Client(key)
 loc1 = sys.argv[1]
 loc2 = sys.argv[2]
 
-def checkIfClose(loc1, loc2):
-    distanceMatrix = gmaps.distance_matrix(loc1,loc2)['rows'][0]['elements'][0]
+def checkIfClose(lo1, lo2):
+    distanceMatrix = gmaps.distance_matrix(lo1,lo2)['rows'][0]['elements'][0]
     if distanceMatrix['distance']['value'] <= 1600:
         return True
 
+checkIfClose(loc1, loc2)
