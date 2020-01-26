c067f815c3fa0debbbbe4efda9238c10c2b94917
Sam Boldon <samboldon@gmail.com>
2020-01-26 04:32:03 -0600

    establish working python process!!!

---
+++ location-services/get-address.py
@@ -0,0 +1,12 @@
+import sys
+import os
+from location import Address
+
+
+def main(argv):
+    address = Address()
+    print(address.getAddress())
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
