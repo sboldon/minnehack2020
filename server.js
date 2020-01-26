64b5161f38c4f271cb49afaa2bb477700f71b681
Sam Boldon <samboldon@gmail.com>
2020-01-26 01:45:04 -0600

    attemp domain integration

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const testAPIRouter = require('./routes/test-api');
 
 const app = express();
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 80;
 
 app.use(express.json()); // essentially bodyParser
 
