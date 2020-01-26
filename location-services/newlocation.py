32ee23f65caa082b227fc35b5f6da407ab758e55
Vishnu Kabir Chhabra <chhab018@umn.edu>
2020-01-26 03:20:07 -0600

    Incorporrate Address

--- location-services/newlocation.py
+++ location-services/newlocation.py
@@ -3,9 +3,12 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 key = os.getenv("API_KEY")
-# distance = gm.distance_matrix()
+from location import Address
+import sys
 gmaps = gm.Client(key) 
 my_dist = gmaps.distance_matrix('Delhi','Mumbai')['rows'][0]['elements'][0] 
+loc1 = Address.getAddress()
+loc2 = sys.argv[1]
 def get_distance(loc1,loc2):
     return gmaps.distance_matrix(loc1,loc2)['rows'][0]['elements'][0]
 
