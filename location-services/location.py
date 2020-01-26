3ea7edcc26e5d0c17691f923383cf38eb3d5e383
tsadpbb <tsadpbb@protonmail.com>
2020-01-25 23:52:23 -0600

    play with location

--- location-services/location.py
+++ location-services/location.py
@@ -1,10 +1,19 @@
-import googlemaps
+import googlemaps as gm
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Address:
     def __init__(self):
-        self.key = 'his key'
-        self.client = googlemaps.Client(self.key)
+        self.key = os.getenv("API_KEY") 
+        self.client = gm.Client(self.key)
         self.findlatlong()
     
     def findlatlong(self):
+        self.geoValue = self.client.geolocate()
+        #self.lat = self.geoValue['location']['lat']
+        #self.lng = self.geoValue['location']['lng']
 
+    def getAddress(self):
+        self.myAddress = self.client.reverse_geocode((self.lat, self.lng))
+        return self.myAddress
