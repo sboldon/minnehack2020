8f1c8d32039e2956f7b17af573af2fd9dc7aaaab
tsadpbb <tsadpbb@protonmail.com>
2020-01-31 16:43:30 -0600

    fix it some more by moving import stuff

--- location-services/sendgmail.py
+++ location-services/sendgmail.py
@@ -1,7 +1,6 @@
-import sys
+import smtplib, ssl, sys
 sendGmail(sys.argv[1], sys.argv[2])
 def sendGmail(otherUser, address):
-    import smtplib, ssl
     message = 'I need narcan at' + str(address)
     server = smtplib.SMTP('smpt.gmail.com', 587)
     server.starttls()
