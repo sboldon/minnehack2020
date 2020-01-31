b3fd1d82cb296ae77c6b23175bd0ca2577d5f1f4
tsadpbb <tsadpbb@protonmail.com>
2020-01-31 16:42:04 -0600

    fix send gmail but I'm not sure it will work

--- location-services/sendgmail.py
+++ location-services/sendgmail.py
@@ -1,19 +1,10 @@
-import smtplib, ssl, sys
-
-class sendingEmail:
-    def __init__(self, otherUser, address):
-        self.message = 'I need narcan at ' + str(address)
-        self.smtp_server = 'smpt.gmail.com'
-        self.otherUser = otherUser
-        self.lifePulseEmail = 'lifepulseminnehack@gmail.com'
-        self.lifePulsePassword = 'minnehack2020'
-        self.sendTheMessage()
-    
-    def sendTheMessage(self):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(self.lifePulseEmail, self.lifePulsePassword)
-        server.sendmail(self.lifePulseEmail, self.otherUser, self.message)
-        server.quit()
-
-emailToSend = sendingEmail(sys.argv[1], sys.argv[2])
+import sys
+sendGmail(sys.argv[1], sys.argv[2])
+def sendGmail(otherUser, address):
+    import smtplib, ssl
+    message = 'I need narcan at' + str(address)
+    server = smtplib.SMTP('smpt.gmail.com', 587)
+    server.starttls()
+    server.login('lifepulseminnehack@gmail.com', 'minnehack2020')
+    server.sendmail('lifepulseminnehack@gmail.com', otherUser, message)
+    server.quit()
