8292eb50e9f8be708d90bfeee8c9920a71bd4953
tsadpbb <tsadpbb@protonmail.com>
2020-01-31 17:23:36 -0600

    broke send gmail, possibly

--- location-services/sendgmail.py
+++ location-services/sendgmail.py
@@ -1,9 +1,12 @@
 import smtplib, ssl, sys
 def sendGmail(otherUser, address):
     message = 'I need narcan at' + str(address)
+    lifePulseUser = 'lifepulseminnehack@gmail.com'
+    lifePulsePass = 'minnehack2020'
+    toSend = otherUser
     server = smtplib.SMTP('smpt.gmail.com', 587)
     server.starttls()
-    server.login('lifepulseminnehack@gmail.com', 'minnehack2020')
-    server.sendmail('lifepulseminnehack@gmail.com', otherUser, message)
+    server.login(lifePulseUser, lifePulsePass)
+    server.sendmail(lifePulseUser, toSend, message)
     server.quit()
 sendGmail(sys.argv[1], sys.argv[2])
