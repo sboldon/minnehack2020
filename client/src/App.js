49c993d68b5b4d5cc49820d25addfb8040a53775
Sam Boldon <samboldon@gmail.com>
2020-01-31 20:57:47 -0600

    fix login persistence, add realtime location tracking

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,73 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import base from './models/base';
-import TestApi from './pages/TestApi';
 import Error404 from './pages/Error404';
 import Maps from './pages/Map';
 import Login from './pages/Login';
 import AuthRequired from './pages/AuthRequired';
 
-
 export default class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       success: false,
-      account: {}
-    }
-    this.handleSuccess = this.handleSuccess.bind(this);
+      account: {},
+    };
+    //    this.handleSuccess = this.handleSuccess.bind(this);
     this.LoggedOut = this.LoggedOut.bind(this);
+    this.checkUserStatus = this.checkUserStatus.bind(this);
   }
 
-  handleSuccess(login) {
-    this.setState({success: login});
+  checkUserStatus() {
     base.auth().onAuthStateChanged(user => {
-      console.log(user);
       if (user) {
-        console.log(user);
-        this.setState({uid: user.id})
-        localStorage.setItem('user', user.id)
-        postUser(uid);
-      }
-      else {
-        this.setState({uid: null})
+        this.setState({success: true});
+        localStorage.setItem('user', user.uid);
+      } else {
         localStorage.removeItem('user');
       }
-      console.log(localStorage);
-    })
+      //      console.log(localStorage);
+    });
   }
 
-  componentDidUpdate() {
+  componentDidMount() {
+    this.checkUserStatus();
   }
+
+  // handleSuccess(login) {
+  //  this.setState({success: login});
+  //}
 
   LoggedOut() {
     return (
       <Switch>
-        {/* <Route path="/" exact component={Login} handleSuccess={this.handleSuccess} /> */}
-        <Route path="/" render={props => <Login {...props} handleSuccess={this.handleSuccess} />} />
+        <Route
+          path="/"
+          render={props => (
+            <Login {...props} handleSuccess={this.handleSuccess} />
+          )}
+        />
         <Route path="*" component={AuthRequired} />
       </Switch>
-    )
+    );
   }
 
   LoggedIn() {
-    return(
+    return (
       <Switch>
         <Route path="/" exact component={Maps} />
         <Route path="*" component={Error404} />
       </Switch>
-    )
+    );
   }
 
   render() {
     return (
       <Router>
         <div className="app-container">
-            {this.state.success ? this.LoggedIn() : this.LoggedOut()}
+          {this.state.success ? this.LoggedIn() : this.LoggedOut()}
         </div>
       </Router>
     );
   }
-
 }
-
-
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,57 +1,66 @@
-import React, { useState} from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
 import base from '../models/base';
 
 export default function Login(props) {
   const blankFormContent = {
     email: '',
-    password: ''
-  }
+    password: '',
+  };
 
   const [account, setAccount] = useState(blankFormContent);
 
-  const handleChange = (e) => {
-    setAccount({ ...account, [e.target.name]: e.target.value })
-  }
+  const handleChange = e => {
+    setAccount({...account, [e.target.name]: e.target.value});
+  };
 
-  const signIn = (e) => {
+  const signIn = e => {
     e.preventDefault();
-    base.auth().signInWithEmailAndPassword(account.email, account.password).then((u)=>{
-      props.handleSuccess(true);
-      //setAccount(blankFormContent);
-    }).catch((error) => {
+    base
+      .auth()
+      .signInWithEmailAndPassword(account.email, account.password)
+      .then(() => {
+        props.handleSuccess(true);
+      })
+      .catch(error => {
         console.log(error);
       });
     console.log(account);
-   
-  }
-  
-  const signUp = (e) => {
+  };
+
+  const signUp = e => {
     e.preventDefault();
-    base.auth().createUserWithEmailAndPassword(account.email, account.password).then((u)=>{
-      props.handleSuccess(true);
-      //setAccount(blankFormContent);
-    }).then((u)=>{console.log(u)})
-    .catch((error) => {
-        console.log(error);
+    base
+      .auth()
+      .createUserWithEmailAndPassword(account.email, account.password)
+      .then(() => {
+        props.handleSuccess(true);
+        //setAccount(blankFormContent);
       })
-  }
+      .then(u => {
+        console.log(u);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   return (
     <main>
       <section>
         <div>
-            <header><h1>Sign In</h1></header>
-            <form onSubmit={signIn}>
+          <header>
+            <h1>Sign In</h1>
+          </header>
+          <form onSubmit={signIn}>
             <label htmlFor="email">
               Email address
-              <input 
-                value={account.email} 
-                onChange={handleChange} 
-                type="email" 
-                name="email" 
-                aria-describedby="emailHelp" 
-                placeholder="example@gmail.com" 
+              <input
+                value={account.email}
+                onChange={handleChange}
+                type="email"
+                name="email"
+                aria-describedby="emailHelp"
+                placeholder="example@gmail.com"
               />
             </label>
             <label htmlFor="password" className="password-box">
@@ -67,11 +76,9 @@ export default function Login(props) {
             </label>
             <button type="submit">Sign In</button>
             <button onClick={signUp}>Sign Up</button>
-            </form>
+          </form>
         </div>
       </section>
     </main>
-  )
+  );
 }
-
-
