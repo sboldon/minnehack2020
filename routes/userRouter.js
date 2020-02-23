5b90635061fcd4aa7dcd37cc94c3396fbfc74ba7
Sam Boldon <samboldon@gmail.com>
2020-02-22 22:49:39 -0600

    incorporate geohashing

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const admin = require('firebase-admin');
+const geohash = require('ngeohash');
 require('dotenv').config();
 
 admin.initializeApp({
@@ -17,6 +18,11 @@ const router = express.Router();
 
 // updates db with user's coordinates
 router.post('/update-location', (req, res) => {
+  const hash = geohash.encode(req.body.location.lat, req.body.location.lng);
+  console.log(hash);
+  const loc = geohash.decode(hash);
+  console.log(loc.latitude, loc.longitude);
+
   db.collection('users')
     .doc(req.body.uid)
     .set(req.body.location);
@@ -24,12 +30,36 @@ router.post('/update-location', (req, res) => {
   res.send('location updated');
 });
 
-router.get('/nearby-users', (req, res) => {
-  console.log('dummy-line');
-});
+// router.get('/nearby-users', (req, res) => {
+//   const { maxLat, minLat, maxLng, minLng } = getBoundingBox(req.body.location);
 
-// function getBoundingBox(lat, lng) {
-//   const maxLat
-// }
+//   db.collection('users').where;
+// });
+
+function rad2deg(rad) {
+  return rad * (180 / Math.PI);
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+function getBoundingBox({ lat, lng }) {
+  const radius = 1.60934;
+  const earthRad = 6371;
+  const maxLat = lat + rad2deg(radius / earthRad);
+  const minLat = lat - rad2deg(radius / earthRad);
+  const maxLng =
+    lng + rad2deg(Math.asin(radius / earthRad) / Math.cos(deg2rad(lat)));
+  const minLng =
+    lng - rad2deg(Math.asin(radius / earthRad) / Math.cos(deg2rad(lat)));
+
+  return {
+    maxLat,
+    minLat,
+    maxLng,
+    minLng,
+  };
+}
 
 module.exports = router;
